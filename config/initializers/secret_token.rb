911a52f8be0d4c7cac3676223dcd4e04cdf48f55
Caitlin Maclatte <macleod.caitlin@gmail.com>
2013-08-23 13:47:31 +1000

    At the end of step 3, finished basic formatting
    still directly following railsgirls app tutorial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thingsitried::Application.config.secret_token = '50316ec1ee0fd9c5257f0c7867a6a2060015847030efe9d68975f39121e01e90e03da5b1edbc7fd43dbdef91b58a75cd89750b381ec0b77c42fa79ddd229bf12'
